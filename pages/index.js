0d8dabbea9c6b3b832b674a8d2f32c1caa0659f8
drdynscript <drdynscript@gmail.com>
2019-01-11 13:02:09 +0100

    WIP

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,21 @@
-import { Entity, Scene } from 'aframe-react'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Entity, Scene } from 'aframe-react'
 
-const VRScene = () => (
-    <Scene>
-        <Entity geometry={{primitive: 'box'}} material={{color: 'red'}} position={{x: 0, y: 0, z: -5}}/>
-        <Entity particle-system={{preset: 'snow'}}/>
-        <Entity light={{type: 'point'}}/>
-        <Entity gltf-model={{src: 'virtualcity.gltf'}}/>
-        <Entity text={{value: 'Hello, WebVR!'}}/>
-    </Scene>
-)
+import '../sass/app.scss'
 
-export default VRScene
+export default class extends React.Component {
+  render() {
+    return (
+        <div style={{height: '100%', width: '100%'}}>
+            <Scene>
+                <Entity geometry={{primitive: 'box'}} material={{color: 'red'}} position={{x: 0, y: 0, z: -5}}/>
+                <Entity particle-system={{preset: 'snow'}}/>
+                <Entity light={{type: 'point'}}/>
+                <Entity gltf-model={{src: 'virtualcity.gltf'}}/>
+                <Entity text={{value: 'Hello, WebVR!'}}/>
+            </Scene>
+        </div>
+    )
+  }
+}
